d2d07074bd6f5436a6b6639cea49d6f2fb38048f
Víctor Cardozo <vcardozof@gmail.com>
2021-02-01 16:27:20 +0100

    Adds response status and new methods

--- src/main/java/com/ironhack/controllerdemo/controller/impl/ProductController.java
+++ src/main/java/com/ironhack/controllerdemo/controller/impl/ProductController.java
@@ -1,13 +1,19 @@
 package com.ironhack.controllerdemo.controller.impl;
 
 import com.ironhack.controllerdemo.controller.interfaces.IProductController;
+import com.ironhack.controllerdemo.enums.Category;
+import com.ironhack.controllerdemo.enums.Department;
 import com.ironhack.controllerdemo.model.Product;
 import com.ironhack.controllerdemo.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProductController implements IProductController {
@@ -16,7 +22,19 @@ public class ProductController implements IProductController {
     private ProductRepository productRepository;
 
     @GetMapping("/products")
+    @ResponseStatus(HttpStatus.OK)
     public List<Product> findAll() {
         return productRepository.findAll();
+    }
+
+    @GetMapping("/products/search")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Product> getProductsByCategoryAndDepartment(@RequestParam Optional<String> category, @RequestParam(defaultValue="clothing") String department){
+
+        if(category.isPresent()) {
+            return productRepository.findByCategoryAndDepartment(Category.valueOf(category.get().toUpperCase()), Department.valueOf(department.toUpperCase()));
+        } else {
+            return productRepository.findByDepartment(Department.valueOf(department.toUpperCase()));
+        }
     }
 }
--- src/main/java/com/ironhack/controllerdemo/repository/ProductRepository.java
+++ src/main/java/com/ironhack/controllerdemo/repository/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.ironhack.controllerdemo.repository;
 
+import com.ironhack.controllerdemo.enums.Category;
+import com.ironhack.controllerdemo.enums.Department;
 import com.ironhack.controllerdemo.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    public List<Product> findByCategoryAndDepartment(Category category, Department department);
+    public List<Product> findByDepartment(Department department);
 }
